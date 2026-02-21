b4015bccb5a79c91750cc0617e6fb1fad922d522
Leilamoumou <89880143+Leilamoumou@users.noreply.github.com>
2026-02-21 02:11:26 +0000

    Deploying to gh-pages from @ Leilamoumou/flowfix-capstone@24b0528b98eaeabcba4cb81e0908bf65340d6cbf 🚀

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ const close_chat =  document.getElementById('close_chat');
 //chat bubble front-end user functionality :3
 if (chat_bubble && chat_window){
     chat_bubble.addEventListener('click', function() {
-            chat_window.style.display =  chat_window.style.display === 'none' ? 'block' : 'none'';
+            chat_window.style.display =  chat_window.style.display === 'none' ? 'block' : 'none';
     });
 }
 
@@ -47,16 +47,18 @@ if (close_chat && chat_window){
     close_chat.addEventListener('click', function() {
         chat_window.style.display = 'none';
     });
-
+}
 //Dark mode Toggle function, actual implementation still needed
 const themeToggle = document.getElementById('themeToggle');
-themeToggle.addEventListener('click', function() {
-    const html = document.documentElement;
-    const isDark = html.getAttribute('data-theme') === 'dark';
-    html.setAttribute('data-theme', isDark ? 'light' : 'dark');
-    this.textContent = isDark ? 'Dark Mode' : 'Light Mode';
-
-
-
-});
+if (themeToggle) {
+    themeToggle.addEventListener('click', function() {
+        const html = document.documentElement;
+        const isDark = html.getAttribute('data-theme') === 'dark';
+        html.setAttribute('data-theme', isDark ? 'light' : 'dark');
+        this.textContent = isDark ? 'Dark Mode' : 'Light Mode';
+    });
 }
+
+
+
+
