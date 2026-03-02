87107776b2be423b8db1b5ea946615a9483f795f
Leila Moumou <89880143+Leilamoumou@users.noreply.github.com>
2026-03-02 01:34:27 +0000

    railway debug

--- flowfix-backend/server.js
+++ flowfix-backend/server.js
@@ -6,11 +6,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 
-//firebase admin initialization
-const serviceAccount = require('./serviceAccountKey.json');
-
+//firebase admin initialization, UPDATED to account for encryption.
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId:   process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey:  process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
 });
 
 const db = admin.firestore();
