112bb630febde288fc6653df8d57e1f41ffcd845
judykuang <jkuang2724@bths.edu>
2026-03-02 00:23:51 -0500

    Add nav bar to FAQ page + styling

--- docs/FAQ/FAQ.js
+++ docs/FAQ/FAQ.js
@@ -81,3 +81,47 @@ btn.addEventListener("click", function() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 });
+
+//Dark Mode Toggle Function with Icon Swapping
+const themeToggle = document.getElementById("themeToggle");
+
+if (themeToggle)
+{
+  themeToggle.addEventListener("click", function()
+  {
+    const html = document.documentElement;
+    const themeIcon = this.querySelector("img");
+
+    // Checks if webpage is currently in Dark Mode
+    const isDark = html.getAttribute("data-theme") === "dark";
+
+    if (isDark)
+    {
+      // If webpage is in dark mode, switch to light mode
+      html.setAttribute("data-theme", "light");
+      themeIcon.src = "dark-mode.png";
+      themeIcon.alt = "Switch to Dark Mode"; // Alternative Text for greater accessibility
+    }
+    else
+    {
+      // If webpage is in light mode, switch to dark mode
+      html.setAttribute("data-theme", "dark");
+      themeIcon.src = "light-mode.png";
+      themeIcon.alt = "Switch to Light Mode"; // Alternative Text for greater accessibility
+    }
+  });
+}
+
+// Hamburger Functionality - Toggle open/close
+const hamburgerMenu = document.getElementById('hamburger');
+const mainNav = document.querySelector('.main-nav');
+
+hamburgerMenu.addEventListener('click', function() {
+    mainNav.classList.toggle('active');
+});
+
+document.querySelectorAll('.main-nav a').forEach(function(link) {
+    link.addEventListener('click', function() {
+        mainNav.classList.remove('active');
+    })
+})
