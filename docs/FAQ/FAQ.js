b25ad0ce84da5c9c39dd6ec017556c836dfe8e6d
judykuang <jkuang2724@bths.edu>
2026-04-27 23:38:00 -0400

    Implemented modal functionality in FAQ webpage

--- docs/FAQ/FAQ.js
+++ docs/FAQ/FAQ.js
@@ -125,3 +125,29 @@ document.querySelectorAll('.main-nav a').forEach(function(link) {
         mainNav.classList.remove('active');
     })
 })
+
+// Modal Functionality
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
